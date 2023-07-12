802371222cfccdb8eedd7b5f7579823fc5270819
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-07-12 15:57:37 +0530

    Time: 7 ms (29.19%), Space: 42 MB (16.33%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -8,30 +8,20 @@ class Solution {
         int i = 0, j = 0, k = 0, f = 0;
         while(i < s1+s2 && j < s1 && k < s2) {
             if(f == 0) {
-                res += word1.charAt(j);
-                i++;
-                j++;
+                res += word1.charAt(j++);
                 f = 1;
             } else {
-                res += word2.charAt(k);
-                i++;
-                k++;
+                res += word2.charAt(k++);
                 f = 0;
             }
         }
         
         //System.out.println("char array: "+str);
         
-        while(j < s1) {
-            res += word1.charAt(j);
-            i++;
-            j++;
-        }
-        while(k < s2) {
-            res += word2.charAt(k);
-            i++;
-            k++;
-        }
+        while(j < s1) res += word1.charAt(j++);
+            
+        while(k < s2) res += word2.charAt(k++);
+        
         return res;
     }
 }
