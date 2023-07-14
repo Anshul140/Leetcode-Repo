4d600899cfbd9b0036452b68fe93d68164cac9d4
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-07-14 20:47:34 +0530

    Time: 246 ms (18.26%), Space: 56.3 MB (42.74%) - LeetHub

---
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
+        map<int, int> dp;
+        int ans = 1;
+        
+        for(int el: arr) {
+            if(dp.count(el - difference)) {
+                int prev = dp[el - difference];
+                ans = max(ans, prev+1);
+                dp[el] = prev+1;
+            } else {
+                dp[el] = 1;
+            }
+        }
+        
+        return ans;
+    }
+};
