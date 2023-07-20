b956130fcfe920abd729b89bee1f27af1d4879de
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-07-21 00:27:11 +0530

    Time: 591 ms (5.77%), Space: 21.5 MB (8.30%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
-    // copy pasted solution,will solve once again
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
-        int j = 0;
-
-        for (int i = 0; i < n; i++) {
-            int asteroid = asteroids[i];
-
-            while (j > 0 && asteroids[j - 1] > 0 && asteroid < 0 && asteroids[j - 1] < abs(asteroid)) {
-                j--;
+    vector<int> asteroidCollision(vector<int>& arr) {
+        vector<int> ans;
+        
+        while(true) {
+            // breaking condition, if no possible collision can happen
+            int f = 0;
+            for(int i = 1; i<arr.size(); i++) {
+                if(arr[i] < 0 and arr[i-1] > 0) {
+                    f = 1;
+                    break;
+                }
             }
-
-            if (j == 0 || asteroid > 0 || asteroids[j - 1] < 0) {
-                asteroids[j++] = asteroid;
-            } else if (asteroids[j - 1] == abs(asteroid)) {
-                j--;
+            if(f == 0) return arr;
+            
+            // breaking condition, after all collision
+            int pos = 0, neg = 0;
+            for(int el: arr) {
+                cout<<el<<"  ";
+                if(el > 0) pos++;
+                else if(el < 0) neg++;
             }
+            
+            cout<<endl;
+            if(pos == 0 or neg == 0) return arr;
+            
+            // pos and neg exists
+            stack<int> st;
+            for(int el: arr) {
+                if(st.size() == 0) st.push(el);
+                else {
+                    if(el < 0 and st.top() > 0) {
+                        int temp = st.top();
+                        int v1 = abs(st.top());
+                        int v2 = abs(el);
+                        st.pop();
+                        
+                        if(v1 > v2) st.push(temp);
+                        else if(v1 < v2) st.push(el);
+                    } else {
+                        st.push(el);
+                    }
+                }
+            }
+            
+            arr.clear();
+            while(!st.empty()) {
+                arr.push_back(st.top());
+                st.pop();
+            }
+            reverse(arr.begin(), arr.end());
         }
-
-        vector<int> result(asteroids.begin(), asteroids.begin() + j);
-
-        return result;
+        
+        return arr;
     }
 };
