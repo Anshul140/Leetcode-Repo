5340d68be02216583f682d1338312bf0decc2e47
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-07-29 18:28:18 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (95.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+    bool solve(int i, int j, long p1, long p2, vector<int>& nums, int f) {
+        if(i > j) {
+            return p1 >= p2;
+        }
+        
+        if(f == 0) {
+            return solve(i+1, j, p1+nums[i], p2, nums, 1) or solve(i, j-1, p1+nums[j], p2, nums, 1);
+        } else {
+            return solve(i+1, j, p1, p2+nums[i], nums, 0) && solve(i, j-1, p1, p2+nums[j], nums, 0);
+        }
+    }
     
     bool PredictTheWinner(vector<int>& nums) {
        int n = nums.size();
-        vector<int> dp(n);
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = nums[i];
-            for (int j = i + 1; j < n; j++) {
-                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
-            }
-        }
-
-        return dp[n - 1] >= 0;
+       return solve(0, n-1, 0, 0, nums, 0);
+       // return ans; 
     }
 };
+
+/*
+1 5 2
+p1: 1 2
+p2: 5
+*/
