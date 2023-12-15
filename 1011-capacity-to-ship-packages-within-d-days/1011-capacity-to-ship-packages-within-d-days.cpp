2431b4a0e1ea519e53c6a0def33016e0378afc57
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-12-16 00:40:42 +0530

    Time: 78 ms (5.59%), Space: 31.4 MB (77.67%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        long lo = *max_element(weights.begin(), weights.end()), hi = 1e9, mid, n = weights.size();
+        // cout<<endl<<endl<<endl;
+        int ans;
+        while(lo <= hi) {
+            mid = (lo + hi) >> 1;
+            long cdays = 1, cw = 0, f = 1;
+            for(int i = 0; i<n; i++) {
+                if(weights[i] > mid) {
+                    f = 0;
+                    break;
+                }
+                cw += weights[i];
+                if(cw > mid) {
+                    cdays++;
+                    cw = weights[i];
+                }
+            }
+            
+            if(cdays > days) f = 0;
+            
+            if(f) {
+                ans = mid;
+                hi = mid-1;
+            } else {
+                lo = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
