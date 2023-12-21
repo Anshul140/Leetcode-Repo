b5b580c721c99b9f4c5c83cd9b9fe45404d7b767
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-12-22 00:49:54 +0530

    Time: 267 ms (12.90%), Space: 66.5 MB (87.58%) - LeetHub

---
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    static bool comp(vector<int>& v1, vector<int>& v2) {
+        return v1[0] <= v2[0];
+    }
+    
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), comp);
+        int diff = 0;
+        for(int i = 1; i<points.size(); i++) {
+            // cout<<"x: "<<points[i][0]<<"  y: "<<points[i][1]<<endl;
+            int cd = points[i][0]-points[i-1][0];
+            diff = max(diff, cd);
+        }
+        
+        return diff;
+    }
+};
