4dbaeeb8dad476bc0c70098f1f5282e0f1183bbc
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2023-12-25 15:06:18 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (5.91%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n;
+    vector<int> dp;
+    int solve(int i, string& s) {
+        if(i == n) return 1;
+
+        if(s[i] == '0') return 0;
+
+        if(dp[i] != -1)
+           return dp[i];
+
+        int ans = 0;
+        for(int j = i; j<n; j++) {
+            if(j+1 < n && s[j] == '1') 
+                return dp[i] = solve(j+1, s) + solve(j+2, s);
+            else if(j+1 < n && s[j] == '2' && (s[j+1] - '0') < 7) 
+                return dp[i] = solve(j+1, s) + solve(j+2, s);
+            else
+                return dp[i] = solve(j+1, s);
+        }
+
+        return dp[i];
+    }
+
+    int numDecodings(string s) {
+        n = s.size();
+        dp.assign(5000, -1);
+        return solve(0, s);
+    }
+};
