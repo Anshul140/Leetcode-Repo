aba70978e89059ea9b1351c470579061c576785a
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2024-03-03 23:44:16 +0530

    Time: 16 ms (88.86%), Space: 16.1 MB (28.78%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,37 +2,39 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
-        set<vector<int>> sv;
         sort(nums.begin(), nums.end());
-        
         int n = nums.size();
-        if(n < 4) return ans;
         
-        int sum = 0;
         for(int i = 0; i<n; i++) {
             for(int j = i+1; j<n; j++) {
-                long long x = target, y = nums[i], z = nums[j];
-                long long tt = x - (y + z);
-                
                 int l = j+1, r = n-1;
+                long long int x = nums[i], y = nums[j];
+                long long int rem = target - (x+y);
                 while(l < r) {
-                    if(nums[l] + nums[r] < tt) l++;
-                    else if(nums[l] + nums[r] > tt) r--;
-                    else {
+                    if(nums[l] + nums[r] == rem) {
                         vector<int> temp;
                         temp.push_back(nums[i]);
                         temp.push_back(nums[j]);
                         temp.push_back(nums[l]);
                         temp.push_back(nums[r]);
-                        sv.insert(temp);
+                        ans.push_back(temp);
+                        
+                        while(l < r and nums[l] == nums[l+1]) l++; 
+                        while(l < r and nums[r] == nums[r-1]) r--; 
+                        l++; 
+                        r--;
+                    } else if(nums[l] + nums[r] < rem){
                         l++;
+                    } else {
                         r--;
                     }
                 }
+                
+                while(j < n-1 and nums[j] == nums[j+1]) j++;
             }
+            while(i < n-1 and nums[i] == nums[i+1]) i++;
         }
-        // cout<<"sum: "<<sum<<endl;
-        for(auto it: sv) ans.push_back(it);
+        
         return ans;
     }
 };
