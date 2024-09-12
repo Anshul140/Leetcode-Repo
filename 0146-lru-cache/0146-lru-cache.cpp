e60e6e2c578a41b626791a6de2fac663503be9dd
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2024-09-12 18:42:03 +0530

    Time: 390 ms (21.05%), Space: 170.3 MB (65.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,77 +1,90 @@
-class Node {
-    public:
-        int key;
-        int val;
-        Node* next;
-        Node* front;
+class Node{
+public:
+    int key;
+    int val;
+    Node* next;
+    Node* prev;
 
-        Node(int key, int val) {
-            this->key = key;
-            this->val = val;
-            next = NULL;
-            front = NULL;
-        }
+    Node(int key, int val){
+        this->key = key;
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
 };
 
 class LRUCache {
 public:
-    unordered_map<int, Node*> cache;
+    int sz;
+    int ctr;
     Node* first = new Node(-1, -1);
     Node* last = new Node(-1, -1);
-    int sz;
+    map<int, Node*> mp;
+
+    void add(Node* cur){
+        Node* temp = first->next;
+
+        // first <-> cur
+        first->next = cur;
+        cur->prev = first;
+
+        // cur <-> temp
+        cur->next = temp;
+        temp->prev = cur;
+        mp[cur->key] = cur;
+    }
+
+    void update(int key, int val){
+        Node* old = mp[key];
+        Node* oldPrev = old->prev;
+        Node* oldNext = old->next;
+
+        oldPrev->next = oldNext;
+        oldNext->prev = oldPrev;
+    }
+
+    void remove(){
+        Node* end = last->prev;
+        Node* prevEnd = end->prev;
+        prevEnd->next = last;
+        last->prev = prevEnd;
+        mp.erase(end->key);
+    }
 
     LRUCache(int capacity) {
         sz = capacity;
         first->next = last;
-        last->front = first;
-    }
-    
-    void deleteNode(Node* t) {
-        Node* f = t->front;
-        Node* n = t->next;
-        f->next = n;
-        n->front = f;
-    }
-    
-    void addNode(Node* t) {
-        Node* s = first->next;
-        first->next = t;
-        t->front = first;
-        t->next = s;
-        s->front = t;
+        last->prev = first;
+        mp.clear();
     }
     
     int get(int key) {
-        // key not found
-        if(!cache.count(key)) return -1;
-        
-        // key-found, hence update its location in doubly linked-list
-        Node* t = cache[key];
-        deleteNode(t);
-        addNode(t);
-        cache[key] = first->next;
-        return first->next->val;
+        if(!mp.count(key)) return -1;
+
+        Node* cur = mp[key];
+        Node* prevCur = cur->prev;
+        prevCur->next = cur->next;
+        cur->next->prev = prevCur;
+
+        add(cur);
+        return cur->val;
     }
     
     void put(int key, int value) {
-        // if key-value already exists, then delete current position, and update with new value and position
-        if(cache.count(key)) {
-            Node* t = cache[key];
-            deleteNode(t);
-            cache.erase(t->key);
+        if(!mp.count(key)){
+            if(mp.size() == sz) remove();
+
+            Node* cur = new Node(key, value);
+            add(cur);
+        } else {
+            Node* cur = mp[key];
+            Node* prevCur = cur->prev;
+            prevCur->next = cur->next;
+            cur->next->prev = prevCur;
+
+            cur->val = value;
+            add(cur);
         }
-        
-        // cache size full, hence delete LRU key
-        if(cache.size() == sz) {
-            // delete LRU key
-            cache.erase(last->front->key);
-            deleteNode(last->front);
-        } 
-        
-        // add new key-value pair
-        Node* newNode = new Node(key, value);
-        addNode(newNode);
-        cache[key] = first->next;
     }
 };
 
