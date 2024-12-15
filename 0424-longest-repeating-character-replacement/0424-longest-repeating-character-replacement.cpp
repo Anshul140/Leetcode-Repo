17da8fdb77d3957d2d07cc5cd6970ae83f968569
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2024-12-16 00:04:36 +0530

    Time: 11 ms (33.21%), Space: 11.5 MB (5.15%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -15,17 +15,13 @@ public:
 
         while(j < n) {
             mp[s[j]]++;
-            mxf = max(mxf, maxFrequency(mp));
+            mxf = max(mxf, mp[s[j]]);
             if((j-i+1)-mxf <= k){
                 ans = max(ans, (j - i + 1));
             } else {
-                while((j - i + 1) - mxf > k){
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0) mp.erase(s[i]);
-                    i++;
-                    mxf = maxFrequency(mp);
-                }
-                ans = max(ans, (j - i + 1));
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
             }
             j++;
         }
