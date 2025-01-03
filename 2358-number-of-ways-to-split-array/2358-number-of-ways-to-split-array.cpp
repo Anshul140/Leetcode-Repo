e00508dc37da5843e94a1e2c345d6c43e681ee1f
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2025-01-03 06:45:42 +0530

    Time: 3 ms (58.82%), Space: 104 MB (14.79%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        vector<long long> pre(n), suf(n);
+
+        pre[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            pre[i] += nums[i] + pre[i-1];
+        }
+
+        suf[n-1] = nums[n-1];
+        for(int i = n-2; i>=0; i--) {
+            suf[i] = suf[i+1]+nums[i];
+        }
+
+        for(int i = 0; i<n-1; i++){
+            if(pre[i] >= suf[i+1]) ans++;
+        }
+
+        return ans;
+    }
+};
