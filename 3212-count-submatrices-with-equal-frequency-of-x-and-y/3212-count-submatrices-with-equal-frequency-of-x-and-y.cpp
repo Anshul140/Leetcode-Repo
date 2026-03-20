cdd1a324a0c8d95c26a3e00108317d6f0b57bb9f
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2026-03-20 22:58:59 +0530

    Time: 81 ms (20.47%), Space: 131.1 MB (11.02%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,24 +1,46 @@
-int Sum[2][1001];
-int seenX[2][1001];
 class Solution {
 public:
-    static int numberOfSubmatrices(vector<vector<char>>& grid) {
-        const int r=grid.size(), c=grid[0].size();
-        memset(Sum[0], 0, (c+1)*sizeof(int));
-        memset(Sum[1], 0, (c+1)*sizeof(int));
-        memset(seenX[0], 0, (c+1)*sizeof(int));
-        memset(seenX[1], 0, (c+1)*sizeof(int));
-        int cnt=0;
-        for(int i=0; i<r; i++){
-            bool iEven=(i&1)==0, prv=!iEven;
-            for(int j=0; j<c; j++){
-                const char c=grid[i][j];
-                bool isX=c=='X', isY=c=='Y';
-                Sum[iEven][j+1]=isX-isY+Sum[iEven][j]+Sum[prv][j+1]-Sum[prv][j];
-                seenX[iEven][j+1]=isX+seenX[iEven][j]+seenX[prv][j+1]-seenX[prv][j];
-                cnt+=((Sum[iEven][j+1]==0) & (seenX[iEven][j+1]>0));
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        // prefix sum
+        vector<vector<int>> ps(n+1, vector<int>(m+1));
+
+        // cnt of 'X'
+        vector<vector<int>> cnt(n+1, vector<int>(m+1));
+
+        map<char, int> mp;
+        mp['X'] = 1;
+        mp['Y'] = -1;
+        mp['.'] = 0;
+
+        ps[0][0] = mp[grid[0][0]];
+        if(grid[0][0] == 'X') cnt[0][0] = 1;
+
+        for(int i = 1; i<n; i++) {
+            ps[i][0] += mp[grid[i][0]] + ps[i-1][0];
+            if(cnt[i-1][0] or grid[i][0] == 'X') cnt[i][0] = 1;
+        }
+
+        for(int i = 1; i<m; i++) {
+            ps[0][i] += mp[grid[0][i]] + ps[0][i-1];
+            if(cnt[0][i-1] || grid[0][i] == 'X') cnt[0][i] = 1;
+        }
+
+        for(int i = 1; i<n; i++) {
+            for(int j = 1; j<m; j++) {
+                ps[i][j] += ps[i-1][j] + ps[i][j-1] + mp[grid[i][j]] - ps[i-1][j-1];
+                if(cnt[i-1][j] or cnt[i][j-1] or grid[i][j] == 'X') cnt[i][j] = 1;
             }
         }
-        return cnt;
+
+        int ans = 0;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(ps[i][j] == 0 and cnt[i][j]) ans++;
+            }
+        }
+
+        return ans;
     }
 };
