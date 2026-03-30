aaaefb1b29698a316edb4e4b161256901856e7f0
Anshul140 <71965548+Anshul140@users.noreply.github.com>
2026-03-30 21:14:49 +0530

    Time: 34 ms (9.78%), Space: 85.2 MB (12.54%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -1,25 +1,74 @@
 class Solution {
 public:
+    struct DSU {
+        int n;
+        vector<int> p, s;
+
+        DSU(int N) {
+            n = N;
+            p.resize(n+1);
+            s.resize(n+1);
+
+            for(int i = 0; i<=n; i++) {
+                p[i] = i;
+                s[i] = 1;
+            }
+        }
+
+        int find(int x) {
+            if(p[x] == x) return x;
+            return p[x] = find(p[x]);
+        }
+
+        void unite(int a, int b) {
+            a = find(a), b = find(b);
+            if(a != b) {
+                if(s[a] < s[b]) swap(a, b);
+
+                s[a] += s[b];
+                p[b] = a;
+            }
+        }
+    };
     string findTheString(vector<vector<int>>& lcp) {
-        int n=lcp.size();
-        string ans(n,0);
-        char ch='a';
-        for(int i=0;i<n;i++){
-            if(ans[i]!=0)continue;
-            if(ch>'z')return "";
-            for(int j=i;j<n;j++){
-                if(lcp[i][j])ans[j]=ch;
-            }
-            ch++;
-        }
-        vector<vector<int>>mat(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(ans[i]==ans[j])mat[i][j]=mat[i+1][j+1]+1;
-                else mat[i][j]=0;
-                if(mat[i][j]!=lcp[i][j])return "";
+        int n = lcp.size();
+        DSU dsu(n+1);
+
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(lcp[i][j] > 0) dsu.unite(i, j);
             }
         }
-        return ans;
+
+        string ans(n, '?');
+        char ch = 'a';
+
+        unordered_map<int, char> mp;
+
+        for(int i = 0; i<n; i++) {
+            int p = dsu.find(i);
+            if(!mp.count(p)) {
+                if(ch > 'z') return "";
+                mp[p] = ch++;
+            }
+            ans[i] = mp[p];
+        }
+
+        cout<<"ans: "<<ans<<endl;
+
+        // validations
+        vector<vector<int>> calc(n, vector<int>(n));
+        for(int i = n-1; i>=0; i--) {
+            for(int j = n-1; j>=0; j--) {
+                if(ans[i] == ans[j]) {
+                    calc[i][j] = 1;
+                    if(i+1 < n and j+1 < n)
+                        calc[i][j] += calc[i+1][j+1];
+                }
+            }
+        }
+
+        if(calc == lcp) return ans;
+        return "";
     }
 };
